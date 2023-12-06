3b869f7ff0c6582f17cd45cce47f586c7c8fc2ea
Minorama <dwarfdew@virginmedia.com>
2023-12-06 19:04:21 +0000

    Completed first 3 tasks of Bank

--- src/Bank.java
+++ src/Bank.java
@@ -1,21 +1,43 @@
 import exceptions.AccountNotFoundException;
 import exceptions.InsufficientFundsException;
+import java.util.Scanner;
+import java.io.File;
+//import java.io.FileWriter;
+import java.io.FileNotFoundException;
+
 
 public class Bank {
 
-    // private ?? accounts;
+    private BankAccount[] accounts = new BankAccount[100];
 
-    /*
-    public ?? readAccounts(??) throws ?? {
-        ??;
+    
+    public void readAccounts() throws FileNotFoundException {
+        File file = new File("accounts.txt");
+        Scanner scanner = new Scanner(file);
+        int count = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            int accountID = Integer.parseInt(parts[0]);
+            double currentBalance = Double.parseDouble(parts[1]);
+            if (parts.length == 3) {
+                double interestRate = Double.parseDouble(parts[2]);
+                accounts[count] = new SavingsAccount(accountID, currentBalance, interestRate);
+                count++;    
+            }
+            else{
+                accounts[count] = new BankAccount(accountID, currentBalance);
+                count++;
+            }
+        }
+        scanner.close();
     }
-    */
 
-    /*
-    public ?? getAccounts() {
-        ??;
+    
+    public BankAccount[] getAccounts() {
+        return accounts;
     }
-    */
+    
 
     /*
     public ?? getAccount(??) throws ?? {
