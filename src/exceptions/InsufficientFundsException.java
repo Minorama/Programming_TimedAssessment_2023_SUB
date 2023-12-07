ef6119f6c71fc94f7072d676385cc9119f4354dc
Minorama <dwarfdew@virginmedia.com>
2023-12-07 23:48:22 +0000

    Fixed a redundancy in InsufficientFundsException.java file

--- src/exceptions/InsufficientFundsException.java
+++ src/exceptions/InsufficientFundsException.java
@@ -4,7 +4,7 @@ public class InsufficientFundsException extends RuntimeException {
 
     
     public InsufficientFundsException(double currentBalance, double amountToWithdraw) { //Takes the current balance and amount to withdraw as parameters
-        super(String.format("Insufficient funds for this withdrawal. (Available Balance:%.1f, Amount Requested:%.1f)", currentBalance, amountToWithdraw)); //Constructs an exception with the message and specific current balance and amount to withdraw explaining the problem
+        super(String.format("Insufficient funds for this withdrawal. (Available Balance:%s, Amount Requested:%s)", currentBalance, amountToWithdraw)); //Constructs an exception with the message and specific current balance and amount to withdraw explaining the problem
     }
     
 }
